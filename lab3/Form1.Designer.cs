a93fa5cc59f87f110e85af33ace9ad0f8b605b96
artem <artem@WIN-3KIOGLOJNI4>
2023-10-10 20:08:42 +0900

    Добавьте файлы проекта.

---
+++ lab3/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tb1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            tb2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // tb1
+            // 
+            tb1.Location = new Point(157, 25);
+            tb1.Name = "tb1";
+            tb1.Size = new Size(100, 23);
+            tb1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(7, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Введите первое число";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(8, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Введите второе число";
+            // 
+            // tb2
+            // 
+            tb2.Location = new Point(157, 60);
+            tb2.Name = "tb2";
+            tb2.Size = new Size(100, 23);
+            tb2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(293, 27);
+            button1.Name = "button1";
+            button1.Size = new Size(174, 57);
+            button1.TabIndex = 4;
+            button1.Text = "Ввод";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(tb2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(tb1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Перунов Артём Станиславович, 255-об, Вариант 18";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tb1;
+        private Label label1;
+        private Label label2;
+        private TextBox tb2;
+        private Button button1;
+    }
+}
